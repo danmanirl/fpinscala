05ac9dcabf16d0e95b072317eeb64faa5d3b08e9
Daniel McElroy <mr.daniel.mcelroy@gmail.com>
2016-10-26 08:44:40 +0100

    completed down to 5.3

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -3,6 +3,12 @@ package fpinscala.laziness
 import Stream._
 trait Stream[+A] {
 
+  // 5.1
+  def toList: List[A] = this match {
+    case Cons(h, t) => h() :: t().toList
+    case Empty => Nil
+  }
+
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
@@ -17,11 +23,27 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
+  // 5.2
+  def take(n: Int): Stream[A] = {
+    def rec(as: Stream[A], counter: Int = 0): Stream[A] = as match {
+      case Cons(h, t) if counter < n => cons(h(), rec(t(), counter + 1))
+      case _ => Empty
+    }
+
+    rec(this)
+  }
 
   def drop(n: Int): Stream[A] = sys.error("todo")
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  // 5.3
+  def takeWhile(p: A => Boolean): Stream[A] = {
+    def rec(as: Stream[A]): Stream[A] = as match {
+      case Cons(h, t) if p(h()) => cons(h(), rec(t()))
+      case _ => Empty
+    }
+
+    rec(this)
+  }
 
   def forAll(p: A => Boolean): Boolean = sys.error("todo")
 
